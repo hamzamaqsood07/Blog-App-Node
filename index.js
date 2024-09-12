964a0e1f698393abe9c2a336751f94f3e540b129
Hamza Maqsood <hamzamaqsood072000@gmail.com>
2024-09-12 18:10:54 +0500

    better logging

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.use(express.json());
 app.use('/blogs', blogRouter);
 const port = process.env.PORT || 3000;  //defining port
 //running server on specified port
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+app.listen(port, () => console.log(`Express Server listening on http://localhost:${port}`));
--- startup/db.js
+++ startup/db.js
@@ -4,8 +4,9 @@ import mongoose from "mongoose";
  * The method connects server to the database.
  */
 export const connectToDB = () => {
+  console.log(`Connecting to MongoDB at url: ${process.env.mongoDBURL}`);
   mongoose
     .connect(process.env.mongoDBURL)
-    .then(() => console.log(`Connected to MongoDB at url: ${process.env.mongoDBURL}`))
-    .catch((err) => console.error(err));
+    .then(() => console.log(`Connected to MongoDB`))
+    .catch((err) => console.error(`Failed to connect to mongodb: ${err}`));
 };
